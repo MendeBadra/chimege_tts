a409a011fcb57965a39c98d2e386c45919a1d27d
MendeBadra <mendamar345@gmail.com>
2025-07-31 16:32:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chimege TTS
+
+This repository is an attempt for text-to-speech syntheisis for Mongolian language.
+
---
+++ hello.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def main():
+    print("Hello from chimege-tts!")
+    print("Numpy version:", np.__version__)
+
+
+if __name__ == "__main__":
+    main()
